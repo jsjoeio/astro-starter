67f5da9d70754d0ac6ac78956bf9a722c78f8d87
Joe <joe@coder.com>
2021-12-17 05:17:48 +0000

    chore: update site.js

--- src/data/site.js
+++ src/data/site.js
@@ -1,13 +1,9 @@
 export default {
-    title: `Astro Minimal Starter`,
+    title: `jsjoeio - Astro Starter`,
     description: `This is an example blog made with Astro.`,
-    url: `https://astro-minimal-starter.netlify.app`, // No trailing slash!
+    url: `https://jsjoeio-astro-starter.netlify.app`, // No trailing slash!
     // JSON LD
     name: `Example`,
-    // Facebook URL
-    facebookURL: `https://facebook.com`,
     // Twitter URL
-    twitterURL: `https://twitter.com/jaydanurwin`,
-    // Instagram URL
-    instagramURL: `https://instagram.com/jaydanurwin`,
+    twitterURL: `https://twitter.com/jsjoeio`,
 }
